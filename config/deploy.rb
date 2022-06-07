f1d6b94b4a90e5c31127b1e11e2d8692ea829357
tolatest <ajayirommy@yahoo.com>
2022-06-07 12:17:34 +0000

    add deploy files

---
+++ config/deploy.rb
@@ -0,0 +1,70 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.17.0"
+
+set :application, "sharkapp"
+set :repo_url, "git@github.com:Romok1/sharkapp.git"
+
+set :deploy_user, 'wcmc'
+
+set :scm_username, "Romok1"
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:application)}"
+set :backup_path, "/home/#{fetch(:deploy_user)}/Backup"
+
+set :deploy_via, :remote_cache
+
+set :rvm_type, :user
+set :rvm_ruby_version, '3.0.0'
+
+set :ssh_options, {
+  forward_agent: true,
+}
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", 'config/master.key'
+set :linked_files, %w{config/database.yml }
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
+
+set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor/bundle", "storage", "node_modules", "client/node_modules")
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+
+namespace :configD do
+     desc "create dot env file"
+     task :setupD do
+       on roles(:app) do
+       execute "cd #{shared_path}/config"
+       execute "touch #{shared_path}/config/database.yml"
+      end
+   end
+end
+
+after 'deploy:check:make_linked_dirs', 'configD:setupD'
+
+set :passenger_restart_with_touch, false
